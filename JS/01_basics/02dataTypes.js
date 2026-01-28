35b035b6b009ef9ab39ac4d1a6bea51136da6708
Dev <guptadev531@gmail.com>
2026-01-28 15:46:27 +0530

    Data Types in JS

---
+++ JS/01_basics/02dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all code as newer verison of JS
+
+// alert(3+3); We are using nodejs not browsers
+
+// We have Premitive and Non-Premitive datatypes
+
+/*Premitive Data Type
+1. Number : when we assgin integer value to the variable
+2. String : Everything inside ouble quoted ("") is string
+3. Boolean : Simple true or false is boolean type 
+4. symbol : When we have identify uniqueness mostly used in React
+5. null : we intensionally give variable a null 
+6. Undefined: When we declare a variable but don't assign any value to it.
+*/
+
+/* Non - Premitive Data Types
+1. Object : Its everywhere being used in JavaScript
+*/
+
+// To check the Type of Variable we have typeof to check the type of variable 
+
+console.log(typeof 3);
+console.log(typeof "Dev");
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null); // This is intesiting gives object which can be called as bug in JS
+
+
+
+
+
